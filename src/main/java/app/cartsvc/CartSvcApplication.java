236aeed6356d4d5109d879b5e5b633f273e7c893
LPanov <lubomirpanov99@gmail.com>
2025-11-04 17:20:52 +0200

    first commit

---
+++ src/main/java/app/cartsvc/CartSvcApplication.java
@@ -0,0 +1,13 @@
+package app.cartsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/cartsvc/CartSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package app.cartsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
